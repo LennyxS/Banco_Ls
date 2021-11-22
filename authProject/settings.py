24265522dcfe54bb3bb5450a7f84b95fc44e1171
LennyxS <lennyxsebastian11@gmail.com>
2021-11-22 14:58:59 -0500

    Se agrega archivos para el heroku

--- authProject/settings.py
+++ authProject/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from os import access, times
 from pathlib import Path
 from datetime import timedelta
-import django_heroku
+import django_heroku 
 
 django_heroku.settings(locals())
 
