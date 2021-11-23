1faf8a1884c9933a232f884186cb0b3fe71a5325
LennyxS <lennyxsebastian11@gmail.com>
2021-11-22 20:51:36 -0500

    Configuracion del settings

--- authProject/settings.py
+++ authProject/settings.py
@@ -10,12 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from os import access, times
 from pathlib import Path
 from datetime import timedelta
-import django_heroku 
 
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -159,7 +156,9 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import django_heroku 
 
+django_heroku.settings(locals())
 
 
 
